d7e0147ff606693aa9c4b4c7d4f11dea8c62bd3a
Alex Buchanan <buchanae@gmail.com>
2013-04-25 10:56:33 -0700

    initial

---
+++ sequence_utils.py
@@ -0,0 +1,103 @@
+import re
+import string
+
+
+complement_translation = string.maketrans('ATCG', 'TAGC')
+
+
+def reverse_complement(seq):
+    return string.translate(seq, complement_translation)[::-1]
+
+
+def chunks(l, n):
+    '''Yield successive n-sized chunks from l.'''
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+
+def codons(seq):
+    return (chunk for chunk in chunks(seq, 3))
+
+
+def translate(seq):
+    return ''.join(CODON_TO_AMINO_ACID[codon] for codon in codons(seq))
+
+
+def get_transcript_seq(genome_sequence, exons, reverse=False):
+    '''Exons are expected to be 1-based closed intervals'''
+
+    exons = sorted(exons, key=lambda e: e.start)
+    seq = ''.join(genome_sequence[exon.start - 1:exon.end] for exon in exons)
+    if reverse:
+        seq = reverse_complement(seq)
+    return seq
+
+
+STOP_CODONS = ('TAA', 'TAG', 'TGA')
+
+CODON_TO_AMINO_ACID = {
+    'ATA': 'T', # Isoleucine
+    'ATC': 'T', # Isoleucine
+    'ATT': 'T', # Isoleucine
+    'ATG': 'M', # Methionine
+    'ACA': 'T', # Threonine
+    'ACC': 'T', # Threonine
+    'ACG': 'T', # Threonine
+    'ACT': 'T', # Threonine
+    'AAC': 'N', # Asparagine
+    'AAT': 'N', # Asparagine
+    'AAA': 'K', # Lysine
+    'AAG': 'K', # Lysine
+    'AGC': 'S', # Serine#Valine
+    'AGT': 'S', # Serine
+    'AGA': 'R', # Arginine
+    'AGG': 'R', # Arginine
+    'CTA': 'L', # Leucine
+    'CTC': 'L', # Leucine
+    'CTG': 'L', # Leucine
+    'CTT': 'L', # Leucine
+    'CCA': 'P', # Proline
+    'CAT': 'H', # Histidine
+    'CAA': 'Q', # Glutamine
+    'CAG': 'Q', # Glutamine
+    'CGA': 'R', # Arginine
+    'CGC': 'R', # Arginine
+    'CGG': 'R', # Arginine
+    'CGT': 'R', # Arginine
+    'CCC': 'P', # Proline
+    'CCG': 'P', # Proline
+    'CCT': 'P', # Proline
+    'CAC': 'H', # Histidine
+    'GTA': 'V', # Valine
+    'GTC': 'V', # Valine
+    'GTG': 'V', # Valine
+    'GTT': 'V', # Valine
+    'GCA': 'A', # Alanine
+    'GCC': 'A', # Alanine
+    'GCG': 'A', # Alanine
+    'GCT': 'A', # Alanine
+    'GAC': 'D', # Aspartic Acid
+    'GAT': 'D', # Aspartic Acid
+    'GAA': 'E', # Glutamic Acid
+    'GAG': 'E', # Glutamic Acid
+    'GGA': 'G', # Glycine
+    'GGC': 'G', # Glycine
+    'GGG': 'G', # Glycine
+    'GGT': 'G', # Glycine
+    'TCA': 'S', # Serine
+    'TCC': 'S', # Serine
+    'TCG': 'S', # Serine
+    'TCT': 'S', # Serine
+    'TTC': 'F', # Phenylalanine
+    'TTT': 'F', # Phenylalanine
+    'TTA': 'L', # Leucine
+    'TTG': 'L', # Leucine
+    'TAC': 'Y', # Tyrosine
+    'TAT': 'Y', # Tyrosine
+    'TAA': '_', # Stop
+    'TAG': '_', # Stop
+    'TGC': 'C', # Cysteine
+    'TGT': 'C', # Cysteine
+    'TGA': '_', # Stop
+    'TGG': 'W', # Tryptophan
+}
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='sequence_utils',
+    description='Small collection of utilities for working with genomic sequences.',
+    long_description=open('README.md').read(),
+    version='0.1',
+    author='Alex Buchanan',
+    author_email='buchanae@gmail.com',
+    license='Apache',
+    py_modules=['sequence_utils']
+)
